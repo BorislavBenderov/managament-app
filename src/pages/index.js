f0b1417ef772f939075f32def0724ed270101206
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-15 17:02:22 +0200

    Export components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,3 @@
-export { Login } from './login/Login';
+export { Login } from "./login/Login";
+export { Register } from "./register/Register";
+export { Management } from "./management/Management";
