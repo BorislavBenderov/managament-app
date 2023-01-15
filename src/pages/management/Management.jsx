2c3db8bf1c2e1364a286b1d1823c52f325c40f20
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-15 17:00:35 +0200

    Add Management component

---
+++ src/pages/management/Management.jsx
@@ -0,0 +1,148 @@
+import { useState } from "react";
+import { CreateUser, EditUser } from "../../components";
+import { DeleteUser } from "../../components/delete-user/DeleteUser";
+import { UserDetails } from "../../components/user-details/UserDetails";
+import { User } from "../../components/user/User";
+
+export const Management = () => {
+  const [createClick, setCreateClick] = useState(false);
+  const [editClick, setEditClick] = useState(false);
+  const [deleteClick, setDeleteClick] = useState(false);
+  const [detailsClick, setDetailsClick] = useState(false);
+
+  return (
+    <>
+      <div className="relative overflow-y-scroll bg-white overflow-auto w-full mb-1 h-screen">
+        {createClick && <CreateUser />}
+        {editClick && <EditUser />}
+        {deleteClick && <DeleteUser />}
+        {detailsClick && <UserDetails />}
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                First name
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z"
+                  ></path>
+                </svg>
+              </th>
+              <th>
+                Last name
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z"
+                  ></path>
+                </svg>
+              </th>
+              <th>
+                Email
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z"
+                  ></path>
+                </svg>
+              </th>
+              <th>
+                Phone
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z"
+                  ></path>
+                </svg>
+              </th>
+              <th>
+                Insurance Price
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="arrow-down"
+                  className="icon active-icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z"
+                  ></path>
+                </svg>
+              </th>
+              <th>
+                Paid
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z"
+                  ></path>
+                </svg>
+              </th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <User
+              setEditClick={setEditClick}
+              setDetailsClick={setDetailsClick}
+              setDeleteClick={setDeleteClick}
+            />
+          </tbody>
+        </table>
+      </div>
+      <button className="btn-add btn" onClick={() => setCreateClick(true)}>
+        Add new user
+      </button>
+    </>
+  );
+};
