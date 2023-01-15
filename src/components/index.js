f590d8e4ab7ac0b97c99a9fafd08d57a6f77bf44
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-15 17:03:07 +0200

    Export component

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { CreateUser } from "./create-user/CreateUser";
+export { EditUser } from "./edit-user/EditUser";
+export { DeleteUser } from "./delete-user/DeleteUser";
+export { User } from "./user/User";
+export { UserDetails } from "./user-details/UserDetails";
