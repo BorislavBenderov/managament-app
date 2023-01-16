25a4376400f387599a77b3adce2493af93dbda14
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-16 10:19:19 +0200

    Add possibility users to edit data

--- src/components/edit-user/EditUser.jsx
+++ src/components/edit-user/EditUser.jsx
@@ -1,4 +1,41 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { useContext, useState } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import { database } from "../../firebaseConfig";
+
 export const EditUser = () => {
+  const { users, editClick, removeClickHandler } = useContext(UserContext);
+  const user = users.find((user) => user.id === editClick);
+  const [values, setValues] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    phoneNumber: user.phoneNumber,
+    insurancePrice: user.insurancePrice,
+    paid: user.paid,
+  });
+
+  const changeHandler = (e) => {
+    setValues((oldState) => ({
+      ...oldState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onEdit = (e) => {
+    e.preventDefault();
+
+    const userData = Object.fromEntries(new FormData(e.target));
+
+    updateDoc(doc(database, "files", user.id), userData)
+      .then(() => {
+        removeClickHandler();
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <div className="overlay">
       <div className="backdrop"></div>
@@ -6,7 +43,7 @@ export const EditUser = () => {
         <div className="user-container">
           <header className="headers">
             <h2>Edit User</h2>
-            <button className="btn close">
+            <button className="btn close" onClick={removeClickHandler}>
               <svg
                 aria-hidden="true"
                 focusable="false"
@@ -24,7 +61,7 @@ export const EditUser = () => {
               </svg>
             </button>
           </header>
-          <form>
+          <form onSubmit={onEdit}>
             <div className="form-row">
               <div className="form-group">
                 <label htmlFor="firstName">First name</label>
@@ -36,6 +73,8 @@ export const EditUser = () => {
                     id="firstName"
                     name="firstName"
                     type="text"
+                    value={values.firstName}
+                    onChange={(e) => changeHandler(e)}
                   />
                 </div>
                 <p className="form-error">
@@ -48,7 +87,13 @@ export const EditUser = () => {
                   <span>
                     <i className="fa-solid fa-user"></i>
                   </span>
-                  <input id="lastName" name="lastName" type="text" />
+                  <input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    value={values.lastName}
+                    onChange={(e) => changeHandler(e)}
+                  />
                 </div>
                 <p className="form-error">
                   Last name should be at least 3 characters long!
@@ -63,7 +108,13 @@ export const EditUser = () => {
                   <span>
                     <i className="fa-solid fa-envelope"></i>
                   </span>
-                  <input id="email" name="email" type="text" />
+                  <input
+                    id="email"
+                    name="email"
+                    type="text"
+                    value={values.email}
+                    onChange={(e) => changeHandler(e)}
+                  />
                 </div>
                 <p className="form-error">Email is not valid!</p>
               </div>
@@ -73,7 +124,13 @@ export const EditUser = () => {
                   <span>
                     <i className="fa-solid fa-phone"></i>
                   </span>
-                  <input id="phoneNumber" name="phoneNumber" type="text" />
+                  <input
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    type="text"
+                    value={values.phoneNumber}
+                    onChange={(e) => changeHandler(e)}
+                  />
                 </div>
                 <p className="form-error">Phone number is not valid!</p>
               </div>
@@ -81,22 +138,34 @@ export const EditUser = () => {
 
             <div className="form-row">
               <div className="form-group">
-                <label htmlFor="email">Insurance Price</label>
+                <label htmlFor="insurancePrice">Insurance Price</label>
                 <div className="input-wrapper">
                   <span>
                     <i className="fa-solid fa-envelope"></i>
                   </span>
-                  <input id="email" name="email" type="text" />
+                  <input
+                    id="insurancePrice"
+                    name="insurancePrice"
+                    type="text"
+                    value={values.insurancePrice}
+                    onChange={(e) => changeHandler(e)}
+                  />
                 </div>
-                <p className="form-error">Email is not valid!</p>
+                <p className="form-error">insurancePrice is not valid!</p>
               </div>
               <div className="form-group">
-                <label htmlFor="phoneNumber">Paid</label>
+                <label htmlFor="paid">Paid</label>
                 <div className="input-wrapper">
                   <span>
                     <i className="fa-solid fa-phone"></i>
                   </span>
-                  <input id="phoneNumber" name="phoneNumber" type="text" />
+                  <input
+                    id="paid"
+                    name="paid"
+                    type="text"
+                    value={values.paid}
+                    onChange={(e) => changeHandler(e)}
+                  />
                 </div>
                 <p className="form-error">Phone number is not valid!</p>
               </div>
@@ -105,11 +174,7 @@ export const EditUser = () => {
               <button id="action-save" className="btn" type="submit">
                 Edit
               </button>
-              <button
-                id="action-cancel"
-                className="btn"
-                type="button"
-              >
+              <button id="action-cancel" className="btn" type="button" onClick={removeClickHandler}>
                 Cancel
               </button>
             </div>
