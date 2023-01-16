d07355fbad8d85e741685bc06cf6b4d5de7f900e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-16 09:34:45 +0200

    Wrap app with context providers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { UserContextProvider } from "./contexts/UserContext";
 import { Login, Management, Register } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/management" element={<Management />} />
-      </Routes>
-    </div>
+    <AuthContextProvider>
+      <UserContextProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/management" element={<Management />} />
+          </Routes>
+        </div>
+      </UserContextProvider>
+    </AuthContextProvider>
   );
 }
 
