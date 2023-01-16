0acf01007c8c74570db1b13d8de79da8af7f82ae
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-16 09:51:45 +0200

    Moves click states in UserContext

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { database } from "../firebaseConfig";
+
+export const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [createClick, setCreateClick] = useState(false);
+  const [editClick, setEditClick] = useState(false);
+  const [deleteClick, setDeleteClick] = useState(false);
+  const [detailsClick, setDetailsClick] = useState(false);
+
+  useEffect(() => {
+    const q = query(collection(database, "files"));
+    onSnapshot(q, (snapshot) => {
+      setUsers(
+        snapshot.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        })
+      );
+      setIsLoading(true);
+    });
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{
+        users,
+        isLoading,
+        createClick,
+        setCreateClick,
+        editClick,
+        setEditClick,
+        deleteClick,
+        setDeleteClick,
+        detailsClick,
+        setDetailsClick,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
